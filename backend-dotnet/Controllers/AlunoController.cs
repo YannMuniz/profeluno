de5c23bbfc7759dc81c0fcfef7f7d81e03c6fe61
Eduassis29 <eduassis29@gmail.com>
2026-02-11 00:17:32 -0300

    Subindo arquivos iniciais da api do C#

---
+++ backend-dotnet/Controllers/AlunoController.cs
@@ -0,0 +1,28 @@
+﻿using backend_dotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_dotnet.Controllers
+{
+    public class AlunoController : Controller
+    {
+
+        Aluno aluno = new Aluno
+        {
+            Id = 1,
+            Nome = "João Silva",
+            Idade = 20,
+            Curso = "Engenharia de Software"
+        };
+
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet("obter-aluno")]
+        public IActionResult ObterAluno ()
+        {
+            return aluno != null ? Ok(aluno) : NotFound("Aluno não encontrado");
+        }
+    }
+}
---
+++ backend-dotnet/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_dotnet.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Curso { get; set; }
+    }
+}
